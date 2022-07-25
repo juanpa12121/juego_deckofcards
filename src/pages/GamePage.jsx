d3cca508ce90d6f18b6b63e0778cc0ca4426de16
juanpa12121 <juan_rivera82172@elpoli.edu.co>
2022-07-25 11:20:44 -0500

    Algunas cosas
    * Pintar cartas opcionadas
    *Pintar GANADOR o PERDEDOR

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,29 +1,26 @@
-import NavBar from "../components/NavBar";
 import usePlayers from "../hooks/usePlayers";
 import {v4 as uuidv4} from "uuid";
 
 const GamePage = () => {
 
-  const {player1, player2} = usePlayers();
+  const {player1, player2, dupCardsPlayer1, dupCardsPlayer2, winner} = usePlayers();
   return (
     <>
-      <NavBar/>
         <div className="mt-20 xs:mt-0 pt-1 xs:pt-32 bg-slate-800 container">
           <div className="flex flex-col my-40 xs:my-0 xs:flex-row overflow-y-auto justify-around py-5 text-white">
             <div className="container">
-              <h3>Jugador 1: <span className="text-gray-400">{player1.name}</span></h3>
+              <div className="flex justify-between">
+                <h3>Jugador 1: <span className="text-gray-400">{player1.name}</span> </h3>
+                <h3><span className={winner.player === "player1" ? "text-green-400" : "text-red-400"}>{winner.player === "player1" ? "GANADOR" : winner.player==="player2" ? "PERDEDOR" : ""}</span></h3>
+              </div>
               <div className="border-b border-slate-400 mt-3"></div>
                 <div className="py-2">
                   <h4>Cartas opcionadas</h4>
                   <div className="flex justify-center flex-col sm:flex-row items-center py-4">
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
+                    {/* Recorrer cartas del jugador 1 */}
+                    {dupCardsPlayer1.map(card =>(
+                      <img key={uuidv4()} alt={"card-"+ card.suit.toLowerCase()} className="carta-opcionada" src={card.image}></img>
+                    ))}
                   </div>
               </div>
               <div className="border-b border-slate-400 mt-3"></div>
@@ -32,26 +29,25 @@ const GamePage = () => {
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center cartas-obtenidas">
                   {/* Recorrer cartas del jugador 1 */}
                   {player1.cards.map(card =>(
-                    <img key={uuidv4()} alt={"card-"+ card.suit.toLowerCase()} className="carta-obtenida" src={card.image}></img>
+                    <img loading="lazy" key={uuidv4()} alt={"card-"+ card.suit.toLowerCase()} className="carta-obtenida" src={card.image}></img>
                   ))}
 
                 </div>
               </div>
             </div>
             <div className="container">
-            <h3>Jugador 2: <span className="text-gray-400">{player2.name}</span></h3>
+            <div className="flex justify-between">
+                <h3>Jugador 2: <span className="text-gray-400">{player2.name}</span></h3>
+                <h3><span className={winner.player === "player2" ? "text-green-400" : "text-red-400"}>{winner.player === "player2" ? "GANADOR" : winner.player==="player1" ? "PERDEDOR" : ""}</span></h3>
+              </div>
               <div className="border-b border-slate-400 mt-3"></div>
               <div className="py-2">
                 <h4>Cartas opcionadas</h4>
                 <div className="flex justify-center flex-col sm:flex-row items-center py-4">
-                <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <br></br>
+                    {/* Recorrer cartas del jugador 1 */}
+                    {dupCardsPlayer2.map(card =>(
+                      <img key={uuidv4()} alt={"card-"+ card.suit.toLowerCase()} className="carta-opcionada" src={card.image}></img>
+                    ))}
                 </div>
             </div>
             <div className="border-b border-slate-400 mt-3"></div>
@@ -59,7 +55,7 @@ const GamePage = () => {
               <h4>Cartas obtenidas</h4>
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 justify-items-center cartas-obtenidas">
                 {player2.cards.map(card =>(
-                  <img key={uuidv4()} alt={"card-"+ card.suit.toLowerCase()} className="carta-obtenida" src={card.image}></img>
+                  <img loading="lazy" key={uuidv4()} alt={"card-"+ card.suit.toLowerCase()} className="carta-obtenida" src={card.image}></img>
                 ))}
                 </div>
             </div>
